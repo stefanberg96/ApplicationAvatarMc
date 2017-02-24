8d579e7acb88e595f563962fba544e50ae4273f7
stefan <bergshm@gmail.com>
2017-02-24 19:05:00 +0100

    cleaned up warnings

--- src/main/java/com/stuff/applicationavatarmc/inventoryKeeperOnDeath.java
+++ src/main/java/com/stuff/applicationavatarmc/inventoryKeeperOnDeath.java
@@ -7,13 +7,11 @@ package com.stuff.applicationavatarmc;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
+
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
@@ -24,11 +22,11 @@ import org.bukkit.plugin.java.JavaPlugin;
  * This plugin will drop (10-x)*10% of the items in the inventory where x is the
  * amount of invetoryKeeper items, and keep all other items that are left in the
  * inventory.
- * @author stefan
+ * @author stefanberg96
  */
 public class inventoryKeeperOnDeath extends JavaPlugin implements Listener {
 
-    private static final Material inventoryKeeper = Material.ENDER_PORTAL_FRAME;
+    private static final Material INVENTORY_KEEPER_ITEM = Material.ENDER_PORTAL_FRAME;
 
     @Override
     public void onEnable() {
@@ -40,58 +38,62 @@ public class inventoryKeeperOnDeath extends JavaPlugin implements Listener {
     public void onDeath(PlayerDeathEvent playerDeath) {
         Player player = playerDeath.getEntity();
         PlayerInventory inventory = playerDeath.getEntity().getInventory();
-        final ItemStack itemStack = new ItemStack(inventoryKeeper);
+        final ItemStack itemStack = new ItemStack(INVENTORY_KEEPER_ITEM);
         HashMap<Integer, ItemStack> inventoryKeeperStack
-                = (HashMap<Integer, ItemStack>) inventory.all(inventoryKeeper);
+                = (HashMap<Integer, ItemStack>) inventory.all(INVENTORY_KEEPER_ITEM);
 
         if (!inventoryKeeperStack.isEmpty()) {
             int amount = countAmountOfKeeperItems(inventoryKeeperStack);
             double percentageToRemove = (10 - Math.min(10, amount)) * 0.1;
             ItemStack[] items = inventory.getContents();
             int amountItemsToDrop = (int) percentageToRemove * items.length;
-            removeItems(amountItemsToDrop, inventory); 
+            removeItems(amountItemsToDrop, inventory);
             playerDeath.setKeepInventory(true);
-        }else{
+        } else {
             playerDeath.setKeepInventory(false);
         }
 
     }
+
     /**
      * randomly chooses an item in the inventory and removes it, it doesn't
      * remove any of the inventoryKeeper items
+     *
      * @param amountItemsToDrop the amount of items that have to be dropped
      * @param items the inventory of the player
      */
     private void removeItems(int amountItemsToDrop, PlayerInventory inventory) {
         //I assume that you won't drop inventoryKeeper items on death
-        Random random=new Random();
-        ItemStack[] items=inventory.getContents();
-        for(int i=0;i<amountItemsToDrop;i++){
-            int index=random.nextInt(items.length);
+        Random random = new Random();
+        ItemStack[] items = inventory.getContents();
+        for (int i = 0; i < amountItemsToDrop; i++) {
+            int index = random.nextInt(items.length);
             int j;
-            for(j=0;j<items.length;j++){
-                if(items[j%items.length]!=null &&
-                        items[j%items.length].getType() != inventoryKeeper){
-                    inventory.remove(items[j%items.length]);
-                    items[j%items.length]=null;
+            for (j = 0; j < items.length; j++) {
+                if (items[j % items.length] != null
+                        && items[j % items.length].getType() != INVENTORY_KEEPER_ITEM) {
+                    inventory.remove(items[j % items.length]);
+                    items[j % items.length] = null;
                     break;
                 }
             }
             //can't remove any more items all other items are
             //inventoryKeeper
-            if(j>=items.length){
+            if (j >= items.length) {
                 break;
             }
         }
     }
 
     /**
-     * Counts the amount of Keeper items the player has, and also remove one 
+     * Counts the amount of Keeper items the player has, and also remove one
      * from the player
+     *
      * @param inventoryKeeperStack HashMap containing the index in the inventory
      * and itemStack containing only inventoryKeeper material
      * @return the total amount of keeper items
      */
+    @SuppressWarnings("null")
     private int countAmountOfKeeperItems(
             HashMap<Integer, ItemStack> inventoryKeeperStack) {
         Collection<ItemStack> itemStacks = inventoryKeeperStack.values();
